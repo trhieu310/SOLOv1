44010b794e73c5d206fdf42a163981282094c092
trhieu310 <trunghieu301999@email.com>
2021-11-08 11:38:00 +0700

    update pycocotools

--- configs/solo/solo_r50_fpn_8gpu_1x.py
+++ configs/solo/solo_r50_fpn_8gpu_1x.py
@@ -50,7 +50,7 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'v24-mini/'
+data_root = '../input/v24-mini/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
