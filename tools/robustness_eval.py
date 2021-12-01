ae10aa29237a49f24bba551116e2e97a597cd8c9
trhieu310 <trunghieu301999@email.com>
2021-12-01 09:17:44 +0700

    update config pycocotools

--- tools/robustness_eval.py
+++ tools/robustness_eval.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def print_coco_results(results):
 
-    def _print(result, ap=1, iouThr=None, areaRng='all', maxDets=100):
+    def _print(result, ap=1, iouThr=None, areaRng='all', maxDets=200):
         iStr = ' {:<18} {} @[ IoU={:<9} | \
         area={:>6s} | maxDets={:>3d} ] = {:0.3f}'
 
