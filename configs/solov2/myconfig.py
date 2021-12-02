dfcef5deb5cdcb9aa78e08eb0be1a8fbd67fafbb
nvdtrieu99 <ngtrieu.99@gmail.com>
2021-12-02 11:21:34 +0700

    modify myconfig for 10kdata eval

--- configs/solov2/myconfig.py
+++ configs/solov2/myconfig.py
@@ -57,7 +57,7 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'DocslayoutDataset'
-data_root = '../../input/solov2minidata/'
+data_root = '../../input/10k-datasets-v24/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
