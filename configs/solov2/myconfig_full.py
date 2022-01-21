99f1f0cf167874434e9fcb06c92b976fc248532d
nvdtrieu99 <ngtrieu.99@gmail.com>
2022-01-21 09:21:31 +0700

    modify config_full 36 epochs

--- configs/solov2/myconfig_full.py
+++ configs/solov2/myconfig_full.py
@@ -57,7 +57,7 @@ test_cfg = dict(
     max_per_img=100)
 # dataset settings
 dataset_type = 'DocslayoutDataset'
-data_root = '../../input/doclayout-v24'
+data_root = '../../input/doclayout-v24/'
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
