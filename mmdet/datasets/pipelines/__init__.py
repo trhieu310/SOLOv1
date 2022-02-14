344afe2e4079cf9d5dac360000a0b7ff4733d182
trhieu310 <trunghieu301999@email.com>
2022-02-14 16:46:40 +0700

    remove flip transform

--- mmdet/datasets/pipelines/__init__.py
+++ mmdet/datasets/pipelines/__init__.py
@@ -4,14 +4,26 @@ from .formating import (Collect, ImageToTensor, ToDataContainer, ToTensor,
 from .instaboost import InstaBoost
 from .loading import LoadAnnotations, LoadImageFromFile, LoadProposals
 from .test_aug import MultiScaleFlipAug
+# from .transforms import (Albu, Expand, MinIoURandomCrop, Normalize, Pad,
+#                          PhotoMetricDistortion, RandomCrop, RandomFlip, Resize,
+#                          SegRescale)
+
 from .transforms import (Albu, Expand, MinIoURandomCrop, Normalize, Pad,
-                         PhotoMetricDistortion, RandomCrop, RandomFlip, Resize,
+                         PhotoMetricDistortion, RandomCrop, Resize,
                          SegRescale)
+
+# __all__ = [
+#     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
+#     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
+#     'LoadProposals', 'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad',
+#     'RandomCrop', 'Normalize', 'SegRescale', 'MinIoURandomCrop', 'Expand',
+#     'PhotoMetricDistortion', 'Albu', 'InstaBoost'
+# ]
 
 __all__ = [
     'Compose', 'to_tensor', 'ToTensor', 'ImageToTensor', 'ToDataContainer',
     'Transpose', 'Collect', 'LoadAnnotations', 'LoadImageFromFile',
-    'LoadProposals', 'MultiScaleFlipAug', 'Resize', 'RandomFlip', 'Pad',
+    'LoadProposals', 'MultiScaleFlipAug', 'Resize', 'Pad',
     'RandomCrop', 'Normalize', 'SegRescale', 'MinIoURandomCrop', 'Expand',
     'PhotoMetricDistortion', 'Albu', 'InstaBoost'
 ]
