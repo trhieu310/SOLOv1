b2f69754b890432132602172a142bf724255893d
trhieu310 <trunghieu301999@email.com>
2022-02-14 18:05:31 +0700

    config augments

--- configs/solo/solo_r50_fpn_8gpu_1x_v27_46k.py
+++ configs/solo/solo_r50_fpn_8gpu_1x_v27_46k.py
@@ -71,7 +71,7 @@ test_pipeline = [
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
-            # dict(type='RandomFlip', flip_ratio=0.0),
+            # dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
             dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
@@ -79,7 +79,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=4,
+    imgs_per_gpu=3,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
