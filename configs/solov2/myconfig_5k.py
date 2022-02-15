cfcc5ffcfbaca3a98075a7fcae59bc850ec13b24
nvdtrieu99 <ngtrieu.99@gmail.com>
2022-02-15 15:48:13 +0700

    add all

--- configs/solov2/myconfig_5k.py
+++ configs/solov2/myconfig_5k.py
@@ -66,7 +66,7 @@ train_pipeline = [
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     # dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
+    # dict(type='Pad', size_divisor=32),
     dict(type='DefaultFormatBundle'),
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
 ]
@@ -80,7 +80,7 @@ test_pipeline = [
             dict(type='Resize', keep_ratio=True),
             # dict(type='RandomFlip'),
             dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
+            # dict(type='Pad', size_divisor=32),
             dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
